04383dc36e96102a55caf82640bde8ab9c8a4968
zaka29 <zaka29@gmail.com>
2019-07-08 23:41:17 +0300

    Splash screen

---
+++ BoutTimeGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoutTimeGame
+//
+//  Created by hamster1 on 8/7/19.
+//  Copyright © 2019 hamster1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
